454e2b3f97c53c06eef432938956c746f18d4bd9
Peter Craig <pac4607@rit.edu>
2024-01-10 10:21:09 -0500

    Began adding code for the Fortran version of a genetic algorithm code

---
+++ Fortran/genetic_algorithm.f90
@@ -0,0 +1,14 @@
+program genetic_algorithm
+
+!We begin with some variable declarations
+implicit none
+
+integer :: popsize
+
+popsize = 100
+
+
+
+print *, 'Hello, World, our population size is: ', popsize
+
+end program genetic_algorithm
