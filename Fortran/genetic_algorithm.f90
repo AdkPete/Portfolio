8eba74b4f3ed555b615822a1670383456c49e67a
Peter Craig <pac4607@rit.edu>
2024-01-16 16:46:24 -0500

    Worked on Fortran genetic algorithm code

--- Fortran/genetic_algorithm.f90
+++ Fortran/genetic_algorithm.f90
@@ -4,11 +4,21 @@ program genetic_algorithm
 implicit none
 
 integer :: popsize
+integer :: i
+integer :: AllocateStatus
+real, dimension(:), allocatable :: population
 
 popsize = 100
+allocate(population(popsize) , STAT = AllocateStatus)
 
+if (AllocateStatus /= 0) then
+    STOP "*** Not enough memory to allocate population array ***"
+end if
 
+do i = 1, 100
+    population(i) = 10
+end do
 
-print *, 'Hello, World, our population size is: ', popsize
+print *, 'Hello, World, our population size is: ', population(10)
 
 end program genetic_algorithm
