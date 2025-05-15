90b71b5483400406a0bb0bc844c355ffac38c525
Peter Craig <pac4607@rit.edu>
2025-05-15 13:34:58 -0400

    Added a new script for producing basic N-body simulations

---
+++ NBody/nbody_sim.py
@@ -0,0 +1,10 @@
+'''
+Code to replicate early N-body galaxy simulations.
+Implements a basic tree-code based N-body simulation, wiht modular
+support for stepping and timestep setting schemes.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
